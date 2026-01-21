9eb11139c9fb46f59fd356608835654b633712d5
Nabil Lanten <nabilanten3@gmail.com>
2026-01-22 03:08:05 +0700

    feat: dynamic breadcrumbs

--- app/(authenticated)/layout.tsx
+++ app/(authenticated)/layout.tsx
@@ -16,6 +16,7 @@ import {
 import {cookies} from "next/headers";
 import {jwtDecode} from "jwt-decode";
 import {appConfig} from "../app-config";
+import Breadcrumbs from "@/components/breadcrumbs";
 
 export default async function Layout({children}: {children: React.ReactNode}) {
   const cookie = await cookies();
@@ -51,26 +52,7 @@ export default async function Layout({children}: {children: React.ReactNode}) {
         }>
         <AppSidebar userRole={role as string} name={name} />
         <SidebarInset>
-          <header className="flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-12">
-            <div className="flex items-center gap-2 px-4">
-              <SidebarTrigger className="-ml-1" />
-              <Separator
-                orientation="vertical"
-                className="mr-2 data-[orientation=vertical]:h-4"
-              />
-              <Breadcrumb>
-                <BreadcrumbList>
-                  <BreadcrumbItem className="hidden md:block">
-                    <BreadcrumbLink href="#">Beranda</BreadcrumbLink>
-                  </BreadcrumbItem>
-                  <BreadcrumbSeparator className="hidden md:block" />
-                  <BreadcrumbItem>
-                    <BreadcrumbPage>Data Fetching</BreadcrumbPage>
-                  </BreadcrumbItem>
-                </BreadcrumbList>
-              </Breadcrumb>
-            </div>
-          </header>
+          <Breadcrumbs/>
           <div className="flex flex-1 flex-col">
             <div className="@container/main flex flex-1 flex-col gap-2">
               <div className="p-4">{children}</div>
