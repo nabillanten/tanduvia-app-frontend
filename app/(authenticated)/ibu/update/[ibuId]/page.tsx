2c76f90fc8b9703304e20a1f4ba3656dc3dcb666
Nabil Lanten <nabilanten3@gmail.com>
2026-01-25 15:28:46 +0700

    fix: cors on create users

--- app/(authenticated)/ibu/update/[ibuId]/page.tsx
+++ app/(authenticated)/ibu/update/[ibuId]/page.tsx
@@ -1,14 +1,14 @@
 import fetchWithCredentials from "@/lib/fetchWithCredential";
 import UpdateIbuForm from "./update-ibu-form";
 
-async function findPosyaduById(id: string) {
+async function findIbuById(id: string) {
   const response = await fetchWithCredentials(`/anak/ibu/${id}`);
   return response;
 }
 
 const UpdateIbuPage = async ({params}: {params: Promise<{ibuId: string}>}) => {
   const {ibuId} = await params;
-  const ibu = await findPosyaduById(ibuId);
+  const ibu = await findIbuById(ibuId);
 
   return (
     <div>
